fd8e9876d92b48200799ef65b325b54f2ab9b3a6
Shynkaruk <shynkarykoleg@gmail.com>
2024-07-02 02:12:24 +0300

    Completed Task

--- task3.js
+++ task3.js
@@ -3,3 +3,12 @@
 // Список з елементами знаходиться у файлі index.html
 // Для запуску використовувати Live Server
 // Для перевірки виконання виконати команду node tests/task3.test.js
+
+const mySelect = document.getElementById('dropdown')
+const deleteBtn = document.getElementById('deleteButton')
+
+deleteBtn.addEventListener('click', () => {
+    
+    const selectedOption = mySelect.options[mySelect.selectedIndex]
+    mySelect.removeChild(selectedOption);
+})
--- task4.js
+++ task4.js
@@ -2,3 +2,14 @@
 //  і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
 // Для запуску використовувати Live Server
 // Для перевірки node tests/task4.test.js
+
+const width = document.getElementById('widthValue')
+const height = document.getElementById('heightValue')
+
+const updateInfo = () => {
+    width.innerHTML = `${window.innerWidth}`;
+    height.innerHTML = `${window.innerHeight}`;
+}
+window.addEventListener('resize', updateInfo)
+
+updateInfo();
